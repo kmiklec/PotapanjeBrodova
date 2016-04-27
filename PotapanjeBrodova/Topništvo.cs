b5fbbeade616c662c03d55d3ddb83aebbc642c91
Kristijan Miklec <krmiklec@vsite.hr>
2016-04-27 21:14:27 +0200

    Dodaj Topništvo.cs i TestTopništva.cs

--- PotapanjeBrodova/Topništvo.cs
+++ PotapanjeBrodova/Topništvo.cs
@@ -5,18 +5,48 @@ using System.Text;
 
 namespace PotapanjeBrodova
 {
+    public enum TaktikaGađanja
+    {
+            Napipavanje,
+            Okruživanje,
+            SustavnoUništavanje
+    }
     public class Topništvo
     {
-        public void UputiPucanj(Flota flota)
+        public Topništvo()
+        {
+            PromjeniTaktikuUNapipavanje();
+        }
+        public Polje UputiPucanj(Flota flota)
         {
             throw new NotImplementedException();
         }
 
-        public void ObradiRezultatGađanja()
+        public void ObradiGađanje(RezultatGađanja rezultat)
         {
 
         }
 
+        private void PromjeniTaktikuUNapipavanje()
+        {
+            TrenutnaTaktika = TaktikaGađanja.Napipavanje;
+        }
+
+        private void PromjeniTaktikuUSustavnoUništavanje()
+        {
+            TrenutnaTaktika = TaktikaGađanja.SustavnoUništavanje;
+        }
+
+        private void PromjeniTaktikuUOkruživanje()
+        {
+            TrenutnaTaktika = TaktikaGađanja.Okruživanje;
+        }
+
+        public TaktikaGađanja TrenutnaTaktika
+        {
+            get; private set;
+        }
         private Flota flota;
     }
 }
+()
