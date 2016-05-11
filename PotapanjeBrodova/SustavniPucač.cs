706a95e68f2a32e45234a350ed582d3a7c5a42af
Kristijan Miklec <krmiklec@vsite.hr>
2016-05-11 21:01:29 +0200

    Proširivanje SustavnogPucača

--- PotapanjeBrodova/SustavniPucač.cs
+++ PotapanjeBrodova/SustavniPucač.cs
@@ -7,9 +7,18 @@ namespace PotapanjeBrodova
 {
     public class SustavniPucač : IPucač
     {
+        public SustavniPucač(IEnumerable<Polje> pogođena, Mreža mreža)
+        {
+            pogođenaPolja = new List<Polje>(pogođena);
+            this.mreža = mreža;
+        }
+
         public Polje UputiPucanj()
         {
             throw new NotImplementedException();
         }
+
+        List<Polje> pogođenaPolja;
+        Mreža mreža;
     }
 }
